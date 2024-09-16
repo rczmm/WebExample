4b81539bb8ecc0e8a5abf1277cb9a94e999db526
rczmm <3179623450@qq.com>
2024-09-16 18:18:56 +0800

    新增用户视图对象(UserVO)定义，包含用户基本信息和时间戳。

---
+++ src/main/java/com/rcz/webexample/utils/VO/UserVO.java
@@ -0,0 +1,43 @@
+package com.rcz.webexample.utils.VO;
+
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+
+    private String userNo;
+
+    private String username;
+
+    private String nickname;
+
+    private String address;
+
+    private String avatar;
+
+    private String familyNo;
+
+    private String communityNo;
+
+    private String occupation;
+
+    private String politicalOutlook;
+
+    private String maritalStatus;
+
+    private String gender;
+
+    private int age;
+
+    private String phone;
+
+    private String email;
+
+    private String remark;
+
+    private String createTime;
+
+    private String updateTime;
+
+}
